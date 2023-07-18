897b9811f60d09160a464fa48e994bbe13e3d4b3
Xander-Crocker <Xander-Crocker@live.nmit.ac.nz>
2023-07-19 10:36:08 +1200

    readme/wordDoc

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WEB601-Assessment-1
+
+npm install node
+
+npm install express
+
+npm install cors
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
 const cors = require("cors");
+const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(cors());
