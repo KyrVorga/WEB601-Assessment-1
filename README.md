abc2c23d83c05cc424e2e43ea8ff8eb7897a30b7
Xander-Crocker <Xander-Crocker@live.nmit.ac.nz>
2023-07-22 17:29:58 +1200

    bootstap template implemented

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # WEB601-Assessment-1
 
-npm init -y
-
-npm install node
-
-npm install express
-
-npm install cors
-
-npm i --save-dev nodemon
-
-npm i ejs
-(install ejs language support via extensions)
+CSS errors are unused by desktop version. They are for the mobile and tablet versions of the site.
 
 To restart server:
 npm run devStart
+
+Packages installed:
+
+npm init -y (initial setup)
+
+npm i node (install node.js)
+
+npm i express (install express.js)
+
+npm i cors (install cors for middleware functionality)
+
+npm i webkit (install webkit for CSS functionality)
+
+npm i --save-dev nodemon (install nodemon and save it to be used as a dependency)
+
+npm i ejs (install ejs language support via extensions)
+
+npm i bootstrap@latest (install bootstraps latest version)
+
+npm i bootstrap jquery --save (install jquery for bootstrap functionality)
+
+npm install express serve-favicon (install express serve-favicon for browser icon)
--- server.js
+++ server.js
@@ -1,19 +1,47 @@
+// import modules
 const express = require('express')
 const app = express()
 const cors = require("cors");
+const favicon = require('serve-favicon');
+const path = require('path');
 const port = 3000
 
+// setting template engine with ejs
 app.set('view engine', 'ejs')
+
+// middleware access to every domain
 app.use(cors());
 
+// Returns a middleware to serve favicon - not working / crashing site
+// app.use(favicon(path.join(__dirname, 'public', 'asset', 'favicon.ico')))
+
+// making the public folder public
+app.use(express.static(path.join(__dirname, 'public')))
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+
+// selected starting page / API endpoint to serve index
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index.ejs')
 })
+// links between pages / API endpoint to serve each page
+app.get('/views/index.ejs', (req, res) => {
+    res.render('index.ejs')
+})
+app.get('/views/post.ejs', (req, res) => {
+    res.render('post.ejs')
+})
+app.get('/views/about.ejs', (req, res) => {
+    res.render('about.ejs')
+})
+app.get('/views/contact.ejs', (req, res) => {
+    res.render('contact.ejs')
+})
+
 
 // start server
 app.listen(port, () => {
     console.log(`App listening at port ${port}`)
 })
-
-// parse requests of content-type - application/json
-app.use(express.json());
